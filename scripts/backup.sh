44183e34ac7fed617765af06b48b946eab7f7d1b
Manikaran Kathuria <manikaran.kathuria@hotstar.com>
2021-08-05 14:07:07 +0530

    Added 2 more test scripts

---
+++ scripts/backup.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+echo "This script runs if the main script fails"
---
+++ scripts/fail_script.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+echo "Returning non zero exit code representing this script has failed."
+
+exit 100
